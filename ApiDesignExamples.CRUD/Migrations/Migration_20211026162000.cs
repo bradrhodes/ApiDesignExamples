8f2a247b575f33b8128c67d0339a037fc8d91c57
BradR <1196817+bradrhodes@users.noreply.github.com>
2021-10-26 16:54:45 -0600

    Remove Cart table from DB and add a column for shipping info to Order table

---
+++ ApiDesignExamples.CRUD/Migrations/Migration_20211026162000.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentMigrator;
+
+namespace ApiDesignExamples.CRUD.Migrations
+{
+    [Migration(20211026162000)]
+    public class Migration_20211026162000 : Migration
+    {
+        public override void Up()
+        {
+            Create.Column("ShippingInfoId").OnTable("Order").AsGuid();
+        }
+
+        public override void Down()
+        {
+            Delete.Column("ShippingInfoId").FromTable("Order");
+        }
+    }
+}
---
+++ ApiDesignExamples.CRUD/Migrations/Migration_20211026162400.cs
@@ -0,0 +1,19 @@
+﻿using FluentMigrator;
+
+namespace ApiDesignExamples.CRUD.Migrations
+{
+    [Migration(20211026162400)]
+    public class Migration_20211026162400 : Migration
+    {
+        public override void Up()
+        {
+            Delete.Table("Cart");
+        }
+
+        public override void Down()
+        {
+            Create.Table("Cart")
+                .WithColumn("Id").AsGuid().NotNullable().PrimaryKey();
+        }
+    }
+}
